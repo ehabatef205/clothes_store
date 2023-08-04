dc3a0ff032a1095e2180e86f6f52058f0893c922
mino-cyber <67506601+mino-cyber@users.noreply.github.com>
2023-08-04 16:39:56 +0300

    edd

--- backend/src/models/ads_slider.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const AdsSlider = new Schema({
-    image:String
-}, { timeseries: true })
-
-
-module.exports = mongoose.model.AdsSlider || mongoose.model("AdsSlider", AdsSlider);
