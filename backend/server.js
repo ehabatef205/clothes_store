08e06d7a872311059ce403fbf0f2a7e784641d6d
ehabatef205 <ehabmahmoud205@gmail.com>
2023-08-04 20:13:33 +0300

    update site

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const fileUpload = require('express-fileupload')
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
 const router = require("./src/routes/index");
 const cors = require("cors");
-const bodyparser = require('body-parser')
 
 const connectDB = async () => {
   try {
@@ -24,10 +22,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(express.json());
-app.use(fileUpload({
-  useTempFiles: true,
-}))
+app.use(express.json({ limit: "50mb" }));
 
 app.use(router);
 
