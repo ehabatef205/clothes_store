1fe63d089bb0df2e62abcdeca53d85dda957657b
ehabatef205 <ehabmahmoud205@gmail.com>
2023-08-05 15:46:06 +0300

    clothes store2

--- backend/src/routes/api/subcategory.js
+++ backend/src/routes/api/subcategory.js
@@ -6,8 +6,10 @@ const router = Router()
 router.get('/', SubCategoryRouter.get_subcategory)
 router.get('/:id', SubCategoryRouter.get_subcategory_by_id)
 router.get('/main_category/:id', SubCategoryRouter.get_subcategory_by_main_category)
+router.get('/main_category2/:id', SubCategoryRouter.get_subcategory_by_main_category2)
 router.post('/', SubCategoryRouter.add_subcategory)
 router.put('/:id', SubCategoryRouter.update_subcategory)
 router.delete('/:id', SubCategoryRouter.delete_subcategory)
+router.put('/view/:id', SubCategoryRouter.UpdateViewSubcategory)
 
 module.exports = router
