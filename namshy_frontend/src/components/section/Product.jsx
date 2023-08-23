606808ac3ae49a84110aba1a54642d9dd63153a6
Maryam Sfwat <71068380+maryammohamed198@users.noreply.github.com>
2023-08-23 14:45:06 +0300

    update

--- namshy_frontend/src/components/section/Product.jsx
+++ namshy_frontend/src/components/section/Product.jsx
@@ -1,3 +1,160 @@
+// import React, { useState, useContext, useEffect ,useRef} from "react";
+// import Carousel from "react-bootstrap/Carousel";
+// import { Card } from "react-bootstrap";
+// import "./homecard.css";
+// import { useNavigate } from "react-router-dom";
+// import * as Cart from "../../api/cart";
+// import * as Wish from "../../api/wish";
+// import { Cookies } from "react-cookie";
+
+// export function Product({ product, index, personal, update_p }) {
+//   const [selectedProduct, setSelectedProduct] = useState(null);
+
+//   const navigate = useNavigate();
+//   const [selectedCardIndex, setSelectedCardIndex] = useState(1);
+//   const [selectedwCardIndex, setSelectedwCardIndex] = useState(1);
+//   const handleButtonClick = (index) => {
+//     setSelectedCardIndex(index);
+//   };
+//   const handlewButtonClick = (index) => {
+//     setSelectedwCardIndex(index);
+//   };
+
+//   const cookie = new Cookies();
+
+//     const handleImageClick = (product) => {
+//         setSelectedProduct(product);
+//         window.scrollTo(0,0)
+//         navigate(`/SelectedProductPage/${product._id}`);
+//     };
+
+//   const addtoBag = async (id) => {
+//     navigate(`/SelectedProductPage/${product._id}`);
+//   };
+//   const addToFavorites = async (id) => {
+//     await Wish.add_cart(id, 1, cookie.get("Auth")).then((e) => {
+//       update_p();
+//     });
+//   };
+
+//   //   console.log("card",product)
+//   const paragrapghstyle={
+//     WebkitLineClamp:1,
+//     WebkitBoxOrient:'vertical',
+//     overflow:'hidden',
+//     display:'-webkit-box',
+    
+//   }
+//   const [isOpen,setIsOpen]=useState(false);
+//   const [showReadMoreButton , setshowReadMoreButton ]=useState(false)
+//   const ref=useRef(null)
+//   useEffect (()=>{
+//     if(ref.current){
+//       setshowReadMoreButton (
+//         ref.current.scrollHeight !== ref.current.clientHeight
+  
+//       )
+//     }
+//   },[])
+//   return (
+//     <div>
+//       <div
+//         className="card m-2 carousel-wrapper"
+//         key={index}
+//         style={{
+//           border:
+//             selectedCardIndex === index
+//               ? "1px solid #d99d2b"
+//               : selectedwCardIndex === index
+//               ? "1px solid red"
+//               : "0.5px solid #C8D2D1",
+//           width: "288px",
+//           height: "320px",
+//         }}
+//       >
+//         <Carousel
+//           controls={false}
+//           style={{ justifyContent: "center" }}
+//           onClick={() => {
+//             handleImageClick(product);
+//           }}
+//         >
+//           {product.imageSrc.map((image, index) => (
+//             <Carousel.Item key={index}>
+//               <img
+//                 className="d-block  "
+//                 style={{
+//                   width: "90%",
+//                   height: "250px",
+//                   margin: "auto",
+//                   borderRadius: "10px",
+//                 }}
+//                 src={image}
+//                 alt={""}
+//               />
+//             </Carousel.Item>
+//           ))}
+//         </Carousel>
+//         <div
+//           className="card-body my-2 d-flex   justify-content-between"
+//           style={{ fontSize: "15px", padding: "5px" }}
+//         >
+//           <div className="d-flex flex-column align-items-start ">
+//             <Card.Title className="mb-0">{product.name}</Card.Title>
+//             <Card.Text className="mb-0">Price: {product.price_after}</Card.Text>
+//           </div>
+//           <div>
+//             <button
+//               className="btn text-light   "
+//               style={{
+//                 backgroundColor: "#d99d2b",
+//                 marginRight: "2px",
+//                 color: "red",
+//               }}
+//               onClick={() => {
+//                 addToFavorites(product._id);
+//                 handlewButtonClick(index);
+//               }}
+//             >
+//               {personal?.wish?.includes(product._id) ? (
+//                 <i className="bi bi-heart-fill fs-5 text-danger"></i>
+//               ) : (
+//                 <i className="bi bi-heart fs-5"></i>
+//               )}
+//             </button>
+
+//             {/* <button
+//               className="btn text-light  "
+//               style={{ backgroundColor: "#d99d2b" }}
+//               onClick={() => {
+//                 addtoBag(product._id);
+//                 handleButtonClick(index);
+//               }}
+//             >
+//               {" "}
+//               {personal?.cart?.includes(product._id) ? (
+//                 <i className="bi bi-check-square-fill fs-5"></i>
+//               ) : (
+//                 <i className="bi bi-cart fs-5"></i>
+//               )}
+//             </button> */}
+//             <button
+//                             className="btn text-light  "
+//                             style={{ backgroundColor: "#d99d2b" }}
+//                             onClick={() => {
+//                                 handleImageClick(product)
+//                             }}
+//                         >  
+//                             <i class="bi bi-eye fs-5"></i>
+//                         </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Product;
 import React, { useState, useContext, useEffect ,useRef} from "react";
 import Carousel from "react-bootstrap/Carousel";
 import { Card } from "react-bootstrap";
@@ -7,20 +164,23 @@ import * as Cart from "../../api/cart";
 import * as Wish from "../../api/wish";
 import { Cookies } from "react-cookie";
 
-export function Product({ product, index, personal, update_p }) {
-  const [selectedProduct, setSelectedProduct] = useState(null);
 
-  const navigate = useNavigate();
-  const [selectedCardIndex, setSelectedCardIndex] = useState(1);
-  const [selectedwCardIndex, setSelectedwCardIndex] = useState(1);
-  const handleButtonClick = (index) => {
-    setSelectedCardIndex(index);
-  };
-  const handlewButtonClick = (index) => {
-    setSelectedwCardIndex(index);
-  };
+export function Product({ product, index ,personal,update_p}) {
+   
+    
+    const [selectedProduct, setSelectedProduct] = useState(null);
+   
+    const navigate = useNavigate();
+    const [selectedCardIndex, setSelectedCardIndex] = useState(1);
+    const [selectedwCardIndex, setSelectedwCardIndex] = useState(1);
+    const handleButtonClick = (index) => {
+        setSelectedCardIndex(index);
+    };
+    const handlewButtonClick = (index) => {
+        setSelectedwCardIndex(index);
+    };
 
-  const cookie = new Cookies();
+    const cookie = new Cookies();
 
     const handleImageClick = (product) => {
         setSelectedProduct(product);
@@ -28,117 +188,113 @@ export function Product({ product, index, personal, update_p }) {
         navigate(`/SelectedProductPage/${product._id}`);
     };
 
-  const addtoBag = async (id) => {
-    navigate(`/SelectedProductPage/${product._id}`);
-  };
-  const addToFavorites = async (id) => {
-    await Wish.add_cart(id, 1, cookie.get("Auth")).then((e) => {
-      update_p();
-    });
-  };
+    const addtoBag = async (id) => {
+        console.log(personal)
+        navigate(`/SelectedProductPage/${product._id}`);
+    };
+    const addToFavorites = async (id) => {
+        
+        console.log(personal);
+        await Wish.add_cart(id, 1, cookie.get("Auth")).then((e) => {
+            update_p()
+        });
+    };
 
-  //   console.log("card",product)
-  const paragrapghstyle={
-    WebkitLineClamp:1,
-    WebkitBoxOrient:'vertical',
-    overflow:'hidden',
-    display:'-webkit-box',
-    
-  }
-  const [isOpen,setIsOpen]=useState(false);
-  const [showReadMoreButton , setshowReadMoreButton ]=useState(false)
-  const ref=useRef(null)
-  useEffect (()=>{
-    if(ref.current){
-      setshowReadMoreButton (
-        ref.current.scrollHeight !== ref.current.clientHeight
-  
-      )
-    }
-  },[])
-  return (
-    <div>
-      <div
-        className="card m-2 carousel-wrapper"
-        key={index}
-        style={{
-          border:
-            selectedCardIndex === index
-              ? "1px solid #d99d2b"
-              : selectedwCardIndex === index
-              ? "1px solid red"
-              : "0.5px solid #C8D2D1",
-          width: "288px",
-          height: "320px",
-        }}
-      >
-        <Carousel
-          controls={false}
-          style={{ justifyContent: "center" }}
-          onClick={() => {
-            handleImageClick(product);
-          }}
-        >
-          {product.imageSrc.map((image, index) => (
-            <Carousel.Item key={index}>
-              <img
-                className="d-block  "
+    //   console.log("card",product)
+    const paragrapghstyle={
+        WebkitLineClamp:1,
+        WebkitBoxOrient:'vertical',
+        overflow:'hidden',
+        display:'-webkit-box',
+        
+      }
+      const [isOpen,setIsOpen]=useState(false);
+      const [showReadMoreButton , setshowReadMoreButton ]=useState(false)
+      const ref=useRef(null)
+      useEffect (()=>{
+        if(ref.current){
+          setshowReadMoreButton (
+            ref.current.scrollHeight !== ref.current.clientHeight
+      
+          )
+        }
+      },[])
+    return (
+        <div>
+            <div
+                className="card m-2 carousel-wrapper"
+                key={index}
                 style={{
-                  width: "90%",
-                  height: "250px",
-                  margin: "auto",
-                  borderRadius: "10px",
+                    border:
+                        selectedCardIndex === index
+                            ? "1px solid #d99d2b"
+                            : selectedwCardIndex === index
+                                ? "1px solid red"
+                                : "0.5px solid #C8D2D1",
+                    width: "288px",
+                    height: "320px",
                 }}
-                src={image}
-                alt={""}
-              />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-        <div
-          className="card-body my-2 d-flex   justify-content-between"
-          style={{ fontSize: "15px", padding: "5px" }}
-        >
-          <div className="d-flex flex-column align-items-start ">
-            <Card.Title className="mb-0">{product.name}</Card.Title>
-            <Card.Text className="mb-0">Price: {product.price_after}</Card.Text>
-          </div>
-          <div>
-            <button
-              className="btn text-light   "
-              style={{
-                backgroundColor: "#d99d2b",
-                marginRight: "2px",
-                color: "red",
-              }}
-              onClick={() => {
-                addToFavorites(product._id);
-                handlewButtonClick(index);
-              }}
             >
-              {personal?.wish?.includes(product._id) ? (
-                <i className="bi bi-heart-fill fs-5 text-danger"></i>
-              ) : (
-                <i className="bi bi-heart fs-5"></i>
-              )}
-            </button>
+                <Carousel
+                    controls={false}
+                    style={{ justifyContent: "center" }}
+                    onClick={() => {
+                        handleImageClick(product);
+                    }}
+                >
+                    {product.imageSrc.map((image, index) => (
+                        <Carousel.Item key={index}>
+                            <img
+                                className="d-block  "
+                                style={{
+                                    width: "90%",
+                                    height: "250px",
+                                    margin: "auto",
+                                    borderRadius: "10px",
+                                }}
+                                src={image}
+                                alt={""}
+                            />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                <div
+                    className="card-body my-2 d-flex   justify-content-between "
+                    style={{ fontSize: "15px", paddingTop: "2px",paddingBottom: "2px",paddingLeft:"5px",paddingRight:"5px" }}
+                >
+                    <div className="d-flex flex-column align-items-start col-7 ">
+                        <Card.Title className="mb-0" style={ isOpen? null: paragrapghstyle} ref={ref}>{product.name}</Card.Title>
+                        <Card.Text className="mb-0" style={ isOpen? null: paragrapghstyle} ref={ref}>Price: {product.price_after}</Card.Text>
+                    </div>
+                    <span
+              className="col-5"
+              style={{ textAlign: "center", " margin-top": "10%" ,paddingTop:"10px"}}
+            >
+                    <div className="justify-content-around d-flex" >
+                        <button
+                            className="btn text-light   "
+                            style={{ backgroundColor:  "#d99d2b",color:"red" }}
+                            onClick={() => {
+                                addToFavorites(product._id);
+                                handlewButtonClick(index);
+                            }}
+                        >{personal?.wish?.includes(product._id)?
+                        <i className="bi bi-heart-fill fs-5 text-danger"></i>
+                        :
+                            <i className="bi bi-heart fs-5"></i>}
+                        </button>
 
-            {/* <button
-              className="btn text-light  "
-              style={{ backgroundColor: "#d99d2b" }}
-              onClick={() => {
-                addtoBag(product._id);
-                handleButtonClick(index);
-              }}
-            >
-              {" "}
-              {personal?.cart?.includes(product._id) ? (
-                <i className="bi bi-check-square-fill fs-5"></i>
-              ) : (
-                <i className="bi bi-cart fs-5"></i>
-              )}
-            </button> */}
-            <button
+                        {/* <button
+                            className="btn text-light  "
+                            style={{ backgroundColor: "#d99d2b" }}
+                            onClick={() => {
+                                addtoBag(product._id);
+                                handleButtonClick(index);
+                            }}
+                        >   {personal?.cart?.includes(product._id)?<i class="bi bi-check-square-fill fs-5"></i>:
+                            <i class="bi bi-cart fs-5"></i>}
+                        </button> */}
+                         <button
                             className="btn text-light  "
                             style={{ backgroundColor: "#d99d2b" }}
                             onClick={() => {
@@ -147,11 +303,12 @@ export function Product({ product, index, personal, update_p }) {
                         >  
                             <i class="bi bi-eye fs-5"></i>
                         </button>
-          </div>
+                    </div>
+                    </span>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Product;
